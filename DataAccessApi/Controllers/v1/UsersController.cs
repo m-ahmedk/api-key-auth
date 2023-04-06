1ecdf06b437033f31d6a0a8670cfeb95b77c674a
Ahmed PC <m.ahmed@braincrop.net>
2023-04-06 11:09:18 +0500

    fix: addressed the issues in create and update and optimize code

--- DataAccessApi/Controllers/v1/UsersController.cs
+++ DataAccessApi/Controllers/v1/UsersController.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
 using Database.Models;
 using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.OpenApi.Models;
 
 namespace DataAccessApi.Controllers.v1
 {
@@ -34,18 +35,27 @@ namespace DataAccessApi.Controllers.v1
             Configuration = _configuration;
         }
 
+        /// <summary>
+        /// Fetch Users
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     GET api/v1/Users/GetUsers
+        /// </remarks>
+        /// <response code="200">Users fetched</response>
+        /// <response code="500">An error occurred while fetching users..</response>
         [HttpGet(Name = "GetUsers")]
-        [ResponseCache(Duration = 10800, Location = ResponseCacheLocation.Any)]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery] int pageNo = 1, int pageSize = 10)
         {
             List<AppUser> Users = new List<AppUser>();
 
             try
             {
-                Users = await _context.AppUsers.ToListAsync();
+                int skip = (pageNo - 1) * pageSize;
+
+                Users = await _context.AppUsers.Skip(skip).Take(pageSize).ToListAsync();
                 return Ok(Users);
             }
-
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
@@ -53,6 +63,15 @@ namespace DataAccessApi.Controllers.v1
             }
         }
 
+        /// <summary>
+        /// Get User by Id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     GET api/v1/Users/GetUser/{id}
+        /// </remarks>
+        /// <response code="200">User fetched</response>
+        /// <response code="500">An error occurred while fetching the user..</response>
         [HttpGet(Name = "GetUser/{id}")]
         [ResponseCache(Duration = 10800, Location = ResponseCacheLocation.Any)]
         public async Task<IActionResult> GetUser(int? id)
@@ -75,16 +94,47 @@ namespace DataAccessApi.Controllers.v1
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
-       
+
+        /// <summary>
+        /// Creates a User.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST api/v1/Users/CreateUser
+        ///     {        
+        ///       "FirstName": "Muhammad Ahmed Villa",
+        ///       "LastName": "Khan",
+        ///       "Email": "m.ahmedk287@gmail.com"
+        ///       "BirthDate": "",
+        ///       "Phone": "string",
+        ///       "Zipcode": "string"
+        ///     }
+        /// </remarks>
+        /// <param name="user"></param>     
+        /// <response code="201">User created</response>
+        /// <response code="500">An error occurred while creating a user..</response>
         [HttpPost(Name = "CreateUser")]
-        public async Task<IActionResult> CreateUser([FromBody] AppUser user)
+        public async Task<IActionResult> CreateUser([FromBody] AppUserDTO user)
         {
             try
             {
-                _context.AppUsers.Add(user);
+                // Map User DTO to an AppUser entity
+                var appUser = new AppUser
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    BirthDate = user.BirthDate,
+                    Email = user.Email,
+                    Phone = user.Phone,
+                    Zipcode = user.Zipcode,
+                    CreatedAt = DateTime.Now
+                };
+
+                _context.AppUsers.Add(appUser);
                 await _context.SaveChangesAsync();
 
-                return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
+                return CreatedAtAction(nameof(GetUser), new { id = appUser.UserId }, appUser);
             }
             catch (Exception ex)
             {
@@ -93,17 +143,32 @@ namespace DataAccessApi.Controllers.v1
             }
         }
 
+        /// <summary>
+        /// Update user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     GET api/v1/Users/UpdateUser/{id}
+        /// </remarks>
+        /// <response code="200">User updated</response>
+        /// <response code="500">An error occurred while updating the user..</response>
         [HttpPut(Name = "UpdateUser/{id}")]
-        public async Task<IActionResult> UpdateUser(int? id, [FromBody] AppUser user)
+        public async Task<IActionResult> UpdateUser(int? id, [FromBody] AppUserDTO user)
         {
             try
             {
-                if (id != user.UserId)
+                AppUser? appUser = 
+                    await _context.AppUsers.Where(u => u.UserId == id).FirstOrDefaultAsync();
+
+
+                if (appUser == null)
                 {
                     return BadRequest();
                 }
 
-                _context.Entry(user).State = EntityState.Modified;
+                appUser = AssignUser(appUser, user);
+
+                _context.Entry(appUser).State = EntityState.Modified;
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
@@ -121,6 +186,15 @@ namespace DataAccessApi.Controllers.v1
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     GET api/v1/Users/DeleteUser/{id}
+        /// </remarks>
+        /// <response code="200">User deleted</response>
+        /// <response code="500">An error occurred while deleting the user..</response>
         [HttpDelete("DeleteUser/{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
@@ -139,6 +213,36 @@ namespace DataAccessApi.Controllers.v1
         private bool UserExists(int? id)
         {
             return _context.AppUsers.Any(e => e.UserId == id);
+        }
+
+        private AppUser AssignUser(AppUser appUser, AppUserDTO user)
+        {
+            if (!(string.IsNullOrEmpty(user.Email)))
+            {
+                appUser.Email = user.Email;
+            }
+
+            if (!(string.IsNullOrEmpty(user.FirstName)))
+            {
+                appUser.FirstName = user.FirstName;
+            }
+
+            if (!(string.IsNullOrEmpty(user.LastName)))
+            {
+                appUser.LastName = user.LastName;
+            }
+
+            if (!(string.IsNullOrEmpty(user.Zipcode)))
+            {
+                appUser.Zipcode = user.Zipcode;
+            }
+
+            if (user.BirthDate!= null)
+            {
+                appUser.BirthDate = user.BirthDate;
+            }
+
+            return appUser;
         }
     }
 }
